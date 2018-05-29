af4ef387a4a7f3dba3f9e8779c8aced0f1ebe2fa
Timur Duehr <tduehr@gmail.com>
2018-05-29 15:59:21 -0500

    aup support

---
+++ support/cas-server-support-aup-couchdb/src/main/java/org/apereo/cas/config/couchdb/AcceptableUsagePolicyConfiguration.java
@@ -0,0 +1,10 @@
+package org.apereo.cas.config.couchdb;
+
+/**
+ * This is {@link AcceptableUsagePolicyConfiguration}.
+ *
+ * @author Timur Duehr
+ * @since 6.0.0
+ */
+public class AcceptableUsagePolicyConfiguration {
+}
